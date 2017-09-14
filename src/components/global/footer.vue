f8859e0796ed61e1374947e7fe96e436a65d68dc
Admin <admin@admins-macbook.local>
2017-09-14 22:01:00 +0200

    Added footer draft

--- src/components/global/footer.vue
+++ src/components/global/footer.vue
@@ -1,23 +1,56 @@
 <style lang="scss">
 
+.footer {
+  background: white;
+  border-top: 1px solid $color-gray;
+  min-height: 400px;
+  clear: left;
+  margin-top: 200px;
+  padding-top: $spacing--larger;
+  padding-bottom: $spacing--larger;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
 
+.input {
+  border-radius: $border-radius;
+  outline: 0;
+  border: 1px solid $color-primary;
+  font-family: $font-secondary;
+  padding: $spacing--smaller;
+  display: inline-block;
+}
 
 </style>
 
 <template lang="html">
 
   <div class="footer">
-    <div class="row">
-      <div class="col-xs-6">
-        <div class="block">
-          <h1>Hei</h1>
-        </div>
-      </div>
-      <div class="col-xs-6">
-        <div class="block">
+    <div class="block">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-md-4">
+          <div class="block --full --mb-larger">
+            <h4>Kontaktinformasjon</h4>
 
+          </div>
+        </div>
+        <div class="col-xs-12 col-md-4">
+          <div class="block --full --mb-larger">
+            <h4>Om Emmasofia</h4>
+
+          </div>
+        </div>
+        <div class="col-xs-12 col-md-4">
+          <div class="block --full --mb-larger">
+            <h4>Hold deg oppdatert</h4>
+            <input class="input" type="email" placeholder="Din epostadresse" />
+            <button class="button">Abonnér</button>
+          </div>
         </div>
       </div>
+    </div>
     </div>
   </div>
 
