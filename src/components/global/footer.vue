21c9eed8a473c4c221772e061f69cab41ebfdaae
vsjoen <vegard@Vegards-MacBook-Air.local>
2020-04-27 04:24:06 +0200

    Removed SVG titles. Text corrections and typo fix.

--- src/components/global/footer.vue
+++ src/components/global/footer.vue
@@ -1,31 +1,32 @@
-<style lang="scss">
-</style>
+<style lang="scss"></style>
 
 <template lang="html">
-
   <div class="footer">
     <div class="block --pt-large --pb-larger">
-
       <div class="container --narrow">
-
         <div class="block --mb-large h2 --text-center">
-          <a href="https://www.facebook.com/emmasofiaorg"><i class="fab fa-facebook-f"></i></a>
-          <a href="https://twitter.com/emmasofiaorg"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.facebook.com/emmasofiaorg"
+            ><i class="fab fa-facebook-f"></i
+          ></a>
+          <a href="https://twitter.com/emmasofiaorg"
+            ><i class="fab fa-twitter"></i
+          ></a>
         </div>
 
         <div class="block --full --text-center text-primary">
           <p>
-            EmmaSofia er en politisk medlemsorganisasjon, som arbeider for å få Norge over til en
-            kunnskapsbasert ruspolitikk. Emma betyr universell og Sofia betyr visdom.
-            Sofia referer til de vises sten.
-          </p>
-          <p>
-            For information in English see our <a class="link" href="https://emmasofia.org">international website</a>.
+            EmmaSofia er en politisk og ideell organisasjon, som startet opp i
+            2015. Emma betyr «universell» og Sofia betyr «visdom». Sofia referer
+            til «de vises sten». Vi er en del av en gryende folkebevegelse som
+            ønsker lovregulering av MDMA og psykedelika med kvalitetskontroll og
+            forbrukervern.
           </p>
         </div>
 
         <div class="block --full --text-center --mt-larger">
-          <router-link :to="{name: 'member'}" class="button --white --large">Støtt oss</router-link>
+          <router-link :to="{ name: 'member' }" class="button --white --large"
+            >Støtt oss</router-link
+          >
         </div>
 
         <div class="block --text-center --mt-larger --full">
@@ -36,12 +37,9 @@
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
   </div>
-
 </template>
 
 <script>
--- src/pages/jobs/index.vue
+++ src/pages/jobs/index.vue
@@ -1,46 +1,44 @@
-<style lang="scss">
-</style>
+<style lang="scss"></style>
 
 <template lang="html">
-
   <main v-show="!loading">
-
-      <div class="hero">
-        <div class="hero__background" />
-        <div class="hero__wrapper">
-          <div class="container --text-center --narrow">
-            <div class="block --full --mb-larger">
-              <div class="hero__title">
-                <h1>Ledige stillinger</h1>
-              </div>
-              <div class="hero__intro">
-                <h3>Her finner du ledige stillinger</h3>
-              </div>
+    <div class="hero">
+      <div class="hero__background" />
+      <div class="hero__wrapper">
+        <div class="container --text-center --narrow">
+          <div class="block --full --mb-larger">
+            <div class="hero__title">
+              <h1>Ledige stillinger</h1>
+            </div>
+            <div class="hero__intro">
+              <h3>Her finner du ledige stillinger</h3>
             </div>
           </div>
         </div>
       </div>
+    </div>
 
-      <section class="block --mt-larger">
-
-        <div class="container --narrow">
-          <div v-if="jobs.length">
-            <router-link :to="{name: 'job', params: {slug: job.slug}}" class="card" :key="job.id" v-for="job in jobs">
-              <div class="block --text-center --pt --pb">
-                <h2>{{job.title}}</h2>
-                <p class="block --full">{{job.shortDesc}}</p>
-              </div>
-            </router-link>
-          </div>
-          <div v-else class="--text-center">
-            <p>Vi har ingen ledige stillinger for øyeblikker.</p>
-          </div>
+    <section class="block --mt-larger">
+      <div class="container --narrow">
+        <div v-if="jobs.length">
+          <router-link
+            :to="{ name: 'job', params: { slug: job.slug } }"
+            class="card"
+            :key="job.id"
+            v-for="job in jobs"
+          >
+            <div class="block --text-center --pt --pb">
+              <h2>{{ job.title }}</h2>
+              <p class="block --full">{{ job.shortDesc }}</p>
+            </div>
+          </router-link>
         </div>
-
-      </section>
-
+        <div v-else class="--text-center">
+          <p>Vi har ingen ledige stillinger for øyeblikket.</p>
+        </div>
+      </div>
+    </section>
   </main>
-
 </template>
 
 <script>
