218c06fe355a904cb2801ced959f5529025b7988
leifriksheim <riksheim.leif@gmail.com>
2020-09-28 14:32:21 +0200

    remove all routes to testkits and drugs

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,6 @@ import FrontPage from "@/pages";
 import Events from "@/pages/events";
 import Media from "@/pages/media";
 import Publications from "@/pages/publications";
-import Testkits from "@/pages/testkits";
-import Testkit from "@/pages/testkits/testkit";
-import HowTo from "@/pages/testkits/how-to";
-import Drugs from "@/pages/drugs";
-import Drug from "@/pages/drugs/drug";
 import About from "@/pages/about-us";
 import Member from "@/pages/member";
 import Jobs from "@/pages/jobs";
@@ -38,36 +33,6 @@ export default new Router({
       path: "/forskning",
       name: "publications",
       component: Publications
-    },
-    {
-      path: "/testkits",
-      name: "testkits",
-      component: Testkits
-    },
-    {
-      path: "/testkits-og-rusvett", // Old route for SEO
-      name: "testkits-old",
-      component: Testkits
-    },
-    {
-      path: "/testkit/:id",
-      name: "testkit",
-      component: Testkit
-    },
-    {
-      path: "/testkits/hvordan-bruke",
-      name: "HowTo",
-      component: HowTo
-    },
-    {
-      path: "/veiledere",
-      name: "drugs",
-      component: Drugs
-    },
-    {
-      path: "/rusmiddel/:slug",
-      name: "drug",
-      component: Drug
     },
     {
       path: "/om-oss",
